4dbf7c4820a4b9cf35f46501e0b3c36b69c6df54
Mohammad Hashemi <89422926+Amahseyn@users.noreply.github.com>
2024-03-20 15:11:41 +0330

    fix new api model and test acc 78

--- test_model.py
+++ test_model.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
 
-data_dir = 'Voices'
+data_dir = 'C:/Users/mhhas/Downloads/Voices'
 classes = os.listdir(data_dir)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -126,31 +126,42 @@ class AudioClassifier (nn.Module):
         # Linear layer
         x = self.lin(x)
 
+        # Final output
         return x
 
 
+# Load your previously saved model
 model = AudioClassifier()
 model.load_state_dict(torch.load('modelclassifier.pth'))
 
-def inference(model, val_dl):  
+# Function to perform inference
+def inference(model, val_dl):
     correct_prediction = 0
     total_prediction = 0
 
+    # Disable gradient calculation
     with torch.no_grad():
         for data in val_dl:
+            # Get the input features and target labels
             inputs, labels = data[0].to(device), data[1].to(device)
 
+            # Normalize the inputs
             inputs_m, inputs_s = inputs.mean(), inputs.std()
             inputs = (inputs - inputs_m) / inputs_s
 
+            # Get predictions
             outputs = model(inputs)
 
+            # Get the predicted class with the highest score
             _, prediction = torch.max(outputs, 1)
 
+            # Count of predictions that matched the target label
             correct_prediction += (prediction == labels).sum().item()
             total_prediction += prediction.shape[0]
 
+    # Calculate accuracy
     acc = correct_prediction / total_prediction
     print(f'Accuracy: {acc:.2f}, Total items: {total_prediction}')
 
+# Perform inference
 inference(model, val_dl)
